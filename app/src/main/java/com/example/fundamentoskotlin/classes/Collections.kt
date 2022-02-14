3df992746eeaa3963d5ff5681ca7ff206de7ffe1
Efrain May <efrain_may_75@hotmail.com>
2022-02-13 18:05:41 -0600

    mutableListOf

--- app/src/main/java/com/example/fundamentoskotlin/classes/Collections.kt
+++ app/src/main/java/com/example/fundamentoskotlin/classes/Collections.kt
@@ -9,4 +9,28 @@ fun main(){
     val frutaList = listOf("Manzana", "Banana", "Uva", "Lima")
     println(frutaList.get((0..frutaList.size-1).random()))
     println("Index de Uva es ${frutaList.indexOf("Uva")}")
+
+    newTopic("Mutable List")
+    val myUser = User(0, "Efrain", "May", Group.FAMILY.ordinal)
+    val bro = myUser.copy(1,"ivan")
+    val friend = bro.copy(id = 2, group = Group.FRIEND.ordinal)
+
+    val usersList = mutableListOf(myUser, bro)
+    println(usersList)
+    usersList.add(friend)
+    println(usersList)
+    //usersList.removeAt(1)
+    usersList.remove(bro)
+    println(usersList)
+
+    val usersSelectedList = mutableListOf<User>()
+    println(usersSelectedList)
+    //usersSelectedList.addAll(usersList)
+    usersSelectedList.add(myUser)
+    println(usersSelectedList)
+    usersSelectedList.set(0, bro)
+    println(usersSelectedList)
+    usersSelectedList.add(myUser)
+    usersSelectedList.add(myUser)
+    println(usersSelectedList)
 }
